971c3068482c8bf44d09253de056ac1675027083
Lukario <LukarioMC@gmail.com>
2015-10-26 13:09:15 -0700

    Made index.html's number system actually sensical, and renamed two folders

--- a-scripts-advanced/a-all-set.sh
+++
@@ -1,5 +0,0 @@
-chmod -R 700 *
-echo "Set all files in this directory so that they can be ran with the command"
-echo "./filename.sh"
-
-ls -l
--- a-scripts-advanced/c-git-pull.sh
+++
@@ -1,4 +0,0 @@
-echo "Must be in the correct directory for your repository"
-echo "Must be have already run b-git-setup.sh"
-
-git pull origin master
